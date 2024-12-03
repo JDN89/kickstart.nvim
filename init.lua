73e206cf31b93bfffc594089c8599c92549ceebe
JDN89 <jan.de.niels@gmail.com>
2024-12-03 21:28:05 +0100

    continiusly enable resize

--- init.lua
+++ init.lua
@@ -185,13 +185,21 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
+-- Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Increase/decrease window height
+vim.keymap.set('n', '<C-Up>', ':resize +1<CR>', { silent = true })
+vim.keymap.set('n', '<C-Down>', ':resize -1<CR>', { silent = true })
+
+-- Increase/decrease window width
+vim.keymap.set('n', '<C-Right>', ':vertical resize +1<CR>', { silent = true })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -1<CR>', { silent = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
