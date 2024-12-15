d8a269d7fb4b408336b13c09676a3114b3b04033
JDN89 <jan.de.niels@gmail.com>
2024-12-15 15:54:59 +0100

    add terminal config and expand telescope searching config

--- init.lua
+++ init.lua
@@ -1,13 +1,5 @@
 require("config.lazy")
 
--- TODO: read:
--- [ ] lsp.txt
--- [ ] treesitter
--- [ ] autocmd.txt
--- [ ] telescope
--- [ ] cmp
--- [ ] conform
-
 vim.opt.shiftwidth = 4
 
 -- VERTI
@@ -17,6 +9,10 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 
 -- Map 'jj' to Escape in insert mode
@@ -29,9 +25,37 @@ vim.wo.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('jan-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('jan-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
+
+-- when you call TermOPen don't set relativenumber
+vim.api.nvim_create_autocmd('TermOpen', {
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    desc = 'Open neovim terminal',
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
+
+    job_id = vim.bo.channel
+})
+
+
+local job_id = 0
+
+-- open a small terminal in the bottom of neovim
+vim.keymap.set("n", "<space>st", function()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
+end
+)
+--example on who to send commands to the terminal when openening the terminal
+vim.keymap.set("n", "<space>example", function()
+    vim.fn.chansend(job_id, { "la\r\n" })
+end)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -94,6 +94,14 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      vim.keymap.set('n', '<space>ep', function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+
+      require "config.telescope.multigrep".setup()
     end,
   },
 }
