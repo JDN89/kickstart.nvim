a1173138aaf5f31854dc8d5f51ae478810d67c3d
JDN89 <jan.de.niels@gmail.com>
2024-12-21 13:01:26 +0100

    disable commandline completion in neovim

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -14,6 +14,13 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'normal'
       },
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        -- optionally disable cmdline completions
+        cmdline = {},
+      },
 
       signature = { enabled = true }
     },
