1d829583bf397a9215733732fb9732e7e0a346ce
JDN89 <jan.de.niels@gmail.com>
2025-02-07 19:53:13 +0100

    cleanup erros

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -117,9 +117,6 @@ return {
       local servers = {
         clangd = {
           on_attach = function()
-            print 'attach for C'
-            require('clangd_extensions.inlay_hints').setup_autocmd()
-            require('clangd_extensions.inlay_hints').set_inlay_hints()
           end,
         },
         gopls = {},
